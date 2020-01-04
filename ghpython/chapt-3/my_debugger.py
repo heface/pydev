fbfd5c77282e4aac2c808665e56023dd2b692608
heface <heface@163.com>
2020-01-04 18:25:52 +0800

    ghpython

--- ghpython/chapt-3/my_debugger.py
+++ ghpython/chapt-3/my_debugger.py
@@ -1,14 +1,20 @@
-#my_debugger.py
+# my_debugger.py
 from ctypes import *
 from my_debugger_defines import *
 kernel32 = windll.kernel32
 
 class debugger():
     def __init__(self):
-        pass
-        
+        self.h_process = None
+        self.pid = None
+        self.debugger_active = False
+        self.h_thread = None
+        self.context = None
+        self.exception = None
+        self.exception_address = None
+                
     def load(self, path_to_exe):
-        #dwCreation flag determinies how to create thr process
+        #dwCreation flag determinies how to create the process
         creation_flags = DEBUG_PROCESS
         startupinfo = STARTUPINFO()
         process_information = PROCESS_INFORMATION()
@@ -20,7 +26,123 @@ class debugger():
             byref(process_information)):
             print("[*] We have successfully launched the process!")
             print("[*] PID:%d" % process_information.dwProcessId)
+            self.h_process = self.open_process(process_information.dwProcessId)
         else:
             print("[*] Error:0x%08x." % kernel32.GetLastError())
+
+    def open_process(self, pid):
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, pid, False)
+        return h_process
+
+    def attach(self,pid):
+        self.h_process = self.open_process(pid)
+        # We attempt to attach to the process
+        # if this fails we exit the call
+        if kernel32.DebugActiveProcess(pid):
+            self.debugger_active = True
+            self.pid = int(pid)
+            self.run()
+        else:
+            print("[*] Unable to attach to the process.")
+
+    def run(self):
+        # Now we have to poll the debuggee for
+        # debugging events
+        while self.debugger_active == True:
+            self.get_debug_event()
+
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status= DBG_CONTINUE
+        if kernel32.WaitForDebugEvent(byref(debug_event),INFINITE):
+            # We aren't going to build any event handlers
+            # just yet. Let's just resume the process for now.
+            input("Press a key to continue...")
+            self.debugger_active = False
+            kernel32.ContinueDebugEvent( \
+                debug_event.dwProcessId, \
+                debug_event.dwThreadId, \
+                continue_status )
+
+    def detach(self):
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print("[*] Finished debugging. Exiting...")
+            return True
+        else:
+            print("There was an error")
+            return False
+
+    def open_thread(self, thread_id):
+        h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
+        if h_thread is not None:
+            return h_thread
+        else:
+            print("[*]Could not obtain a valid thread handle.")
+            return False
+
+    def enumerate_threads(self):
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+        if snapshot is not None:
+            thread_entry.dwSize = sizeof(thread_entry)
+            success = kernel32.Thread32First(snapshot, byref(thread_entry))
+            while success:
+                if thread_entry.th32OwnerProcessID == self.pid:
+                    thread_list.append(thread_entry.th32ThreadID)
+                success = kernel32.Thread32Next(snapshot, byref(thread_entry))
+            kernel32.CloseHandle(snapshot)
+            return thread_list
+
+        else:
+            return False
+
+        
+    def get_thread_context(self, thread_id):
+        context = CONTEXT()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+        h_thread = self.open_thread(thread_id)
+        if kernel32.GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+            return context
+        else:
+            return False
+
+    def get_debug_event(self):
+        debug_event = DEBUG_EVENT()
+        continue_status = DBG_CONTINUE
+        if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
+            self.h_thread = self.open_thread(debug_event.dwThread)
+            self.context = self.get_thread_context(self.h_thread)
+            print("Event Code:%d Thread ID:%d" % \
+                  (debug_event.dwDebugEventCode, debug_event.dwThreadID))
+
+            # If the event code is an exception, we want to
+            # examine it further.
+            if debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT:
+                # Obtain the exception code
+                self.exception = debug_event.u.Exception.ExceptionRecord.ExceptionCode
+                self.exception_address = \
+                    debug_event.u.Exception.ExceptionRecord.ExceptionAddress
+                if exception == EXCEPTION_ACCESS_VIOLATION:
+                    print("Access Violation Detected.")
+                    # If a breakpoint is detected, we call an internal
+                    # handler.
+                elif exception == EXCEPTION_BREAKPOINT:
+                    continue_status = self.exception_handler_breakpoint()
+                elif ec == EXCEPTION_GUARD_PAGE:
+                    print("Guard Page Access Detected.")
+                elif ec == EXCEPTION_SINGLE_STEP:
+                    print("Single Stepping")
             
-      
+            kernel32.ContinueDebugEvent(
+                debug_event.dwProcessId,
+                debug_event.dwThreadId,
+                continue_status)
+            
+
+
+    def exception_handler_breakpoint():
+        print("[*] Inside the breakpoint handler.")
+        print("Exception Address: 0x%08x" % self.exception_address)
+        return DBG_CONTINUE
--- ghpython/readme.md
+++ ghpython/readme.md
@@ -1,4 +1,78 @@
-第一章代码问题：
-printf调用时，传入的字符串需要转换为byte数组，这是因为python3与python2的字符串是不一样的，py2中字符串就是字符数组，原生不支持UTF8；而py3为了支持utf8字符串和py2不一样了。
-第三章代码问题：
-创建进程原文中用的是kernel32.CreateProcessA，需要改成kernel32.CreateProcessW，其实仍然是字符串问题，CreateProcessW是支持宽字符的。
+##第一章代码问题：  
+printf调用时，传入的字符串需要转换为byte数组，这是因为python3与python2的字符串是不一样的，py2中字符串就是字符数组，原生不支持UTF8；而py3为了支持utf8字符串和py2不一样了。  
+##第三章代码问题：  
+创建进程原文中用的是kernel32.CreateProcessA，需要改成kernel32.CreateProcessW，其实仍然是字符串问题，CreateProcessW是支持宽字符的。  
+
+创建进程函数：  
+BOOL WINAPI CreateProcessW(  
+    LPCSTR lpApplicationName,  --执行程序的路径  
+    LPTSTR lpCommandLine,  --传入参数  
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,  
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,  
+    BOOL bInheritHandles,  
+    DWORD dwCreationFlags,  --创建标记  
+    LPVOID lpEnvironment,  
+    LPCTSTR lpCurrentDirectory,  
+    LPSTARTUPINFO lpStartupInfo,  --创建子进程时设置属性  
+    LPPROCESS_INFORMATION lpProcessInformation  --创建进程后接收信息  
+);  
+
+获取进程句柄：  
+HANDLE WINAPI OpenProcess(  
+    DWORD dwDesiredAccess,  --对将要打开的进程拥有什么样的权限  
+    BOOL bInheritHandle，  
+    DWORD dwProcessId  --希望获得句柄的进程 ID  
+);  
+
+附加到目标进程：  
+BOOL WINAPI DebugActiveProcess(  
+    DWORD dwProcessId  
+);  
+
+捕获调试事件：  
+BOOL WINAPI WaitForDebugEvent(  
+    LPDEBUG_EVENT lpDebugEvent,  --调试事件  
+    DWORD dwMilliseconds  --时长，设置为INFINITE（无限等待）  
+);  
+
+调试事件处理后使进程继续执行：  
+BOOL WINAPI ContinueDebugEvent(  
+    DWORD dwProcessId,  --进程ID（WaitForDebugEvent时获取）  
+    DWORD dwThreadId,  --线程ID（WaitForDebugEvent时获取）  
+    DWORD dwContinueStatus  --继续执行(DBG_CONTINUE)或抛出异常(DBG_EXCEPTION_NOT_HANDLED)  
+);  
+
+获取线程句柄：  
+HANDLE WINAPI OpenThread(  
+    DWORD dwDesiredAccess,  
+    BOOL bInheritHandle,  
+    DWORD dwThreadId  --线程标识符  
+);  
+
+枚举所有线程：  
+HANDLE WINAPI CreateToolhelp32Snapshot(  
+    DWORD dwFlags,  --收集的数据类型（线程，进程-TH32CS_SNAPTHREAD，模块，或者堆）  
+    DWORD th32ProcessID  
+);  
+
+BOOL WINAPI Thread32First(  
+    HANDLE hSnapshot,  
+    LPTHREADENTRY32 lpte  
+);  
+
+BOOL WINAPI Thread32Next(  
+    HANDLE hSnapshot,  
+    LPTHREADENTRY32 lpte  
+);  
+
+获取/改变寄存器的值：  
+BOOL WINAPI GetThreadContext(  
+    HANDLE hThread,  
+    LPCONTEXT lpContext  
+);  
+BOOL WINAPI SetThreadContext(  
+    HANDLE hThread,  
+    LPCONTEXT lpContext  
+);  
+
+
